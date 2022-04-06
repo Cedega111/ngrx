def4de6e14c4de32a4af4ade072c08c5ebc8b6cc
dren0r123 <dren0risme@gmail.com>
2022-04-07 00:23:59 +0300

    first()

--- src/app/article-new/components/articles-list/banner-item/banner-item.component.ts
+++ src/app/article-new/components/articles-list/banner-item/banner-item.component.ts
@@ -2,7 +2,7 @@ import { StorageEntityService } from '@app/core/services/storage-entity.service'
 import { StorageFile } from '@app/core/interfaces/storage-file';
 import { Article } from './../../../interfaces/article';
 import { Component, Input, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { filter, first, map, Observable, take, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-banner-item',
@@ -16,6 +16,7 @@ export class BannerItemComponent implements OnInit {
     this.file$ = this.storageEntityService.entities$
     .pipe(
       map(files => files.find(file => file.id === value.previewImage)),
+      first(file => !!file)
     )
   };
 
